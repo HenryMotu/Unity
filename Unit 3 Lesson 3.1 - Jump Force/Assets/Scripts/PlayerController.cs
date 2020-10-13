02c49bdba1bba6e21404382b21d5b2d24d2f2990
HenryMotu <mota_i@i.ua>
2020-10-13 00:25:49 -0400

    Final Unit 3 Lesson 3.1 - Jump Force

---
+++ Unit 3 Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    private Rigidbody playerRb;
+    private Animator playerAnimator;
+    private AudioSource playerAudio;
+    public bool IsGameOver = false;
+    public ParticleSystem ExplosureParticle;
+    public ParticleSystem DirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !IsGameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnimator.SetTrigger("Jump_trig");
+            DirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            DirtParticle.Play();
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            IsGameOver = true;
+            Debug.Log("Game over!");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            ExplosureParticle.Play();
+            DirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1f);
+        }
+    }
+}
