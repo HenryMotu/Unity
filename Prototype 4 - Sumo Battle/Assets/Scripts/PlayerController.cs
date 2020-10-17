643833030069cf501a0a006490faed74f0cf7024
HenryMotu <mota_i@i.ua>
2020-10-16 23:33:44 -0400

    Lesson 4.3 - PowerUp and CountDown

--- Prototype 4 - Sumo Battle/Assets/Scripts/PlayerController.cs
+++ Prototype 4 - Sumo Battle/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float forwardSpeed;
+    public bool hasPowerup = false;
+    private float powerupStrength =  15.0f;
+    public GameObject powerIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +25,39 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * forwardSpeed);
+        powerIndicator.transform.position = (transform.position + new Vector3(0, -0.5f, 0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            RunPowerupRoutine();
+        }
+    }
+
+    private void RunPowerupRoutine()
+    {
+        StartCoroutine("PowerupCountdownRoutine");
+        hasPowerup = true;
+        powerIndicator.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            var awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            Debug.Log("Collided with: " + collision.gameObject.name);
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerIndicator.SetActive(false);
     }
 }
---
+++ Prototype 4 - Sumo Battle/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 randomPos =  GetRandomPosition();
+
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 randomPos;
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
